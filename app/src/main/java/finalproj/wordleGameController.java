15e9753e192067a70957fc80949d228f7f5f8df3
Miyxzu <ezio.hasibuan@gmail.com>
2023-05-24 11:58:18 +0700

    SWITCH STATEMENTS SAVED MY LIFE

--- app/src/main/java/finalproj/wordleGameController.java
+++ app/src/main/java/finalproj/wordleGameController.java
@@ -60,30 +60,163 @@ public class wordleGameController {
 
         if(check1) {
             for (int i = 0; i < guess.length() - 1; i++) {
-                if(guess.charAt(i) == chosenWord.charAt(i)){
-                    
-                }
-                if(chosenWord.indexOf(guess.substring(i, i+1)) != -1){
+                Label letter = getLabelByIndex(i);
 
+                if(guess.charAt(i) == chosenWord.charAt(i)){
+                    letter.setStyle("-fx-alignment: center; -fx-background-color: #528d4e;");
+                } else if(chosenWord.indexOf(guess.substring(i, i+1)) != -1){
+                    letter.setStyle("-fx-alignment: center; -fx-background-color: #b59f3a;");
+                } else {
+                    letter.setStyle("-fx-alignment: center; -fx-background-color: #3a3b3c;");
                 }
             }
+            check1 = false;
+            check2 = true;
         } else if(check2) {
-            
+            for (int i = 0; i < guess.length() - 1; i++) {
+                Label letter = getLabelByIndex(i+5);
+
+                if(guess.charAt(i) == chosenWord.charAt(i)){
+                    letter.setStyle("-fx-alignment: center; -fx-background-color: #528d4e;");
+                } else if(chosenWord.indexOf(guess.substring(i, i+1)) != -1){
+                    letter.setStyle("-fx-alignment: center; -fx-background-color: #b59f3a;");
+                } else {
+                    letter.setStyle("-fx-alignment: center; -fx-background-color: #3a3b3c;");
+                }
+            }
+            check2 = false;
+            check3 = true;
         } else if(check3) {
+            for (int i = 0; i < guess.length() - 1; i++) {
+                Label letter = getLabelByIndex(i+10);
 
+                if(guess.charAt(i) == chosenWord.charAt(i)){
+                    letter.setStyle("-fx-alignment: center; -fx-background-color: #528d4e;");
+                } else if(chosenWord.indexOf(guess.substring(i, i+1)) != -1){
+                    letter.setStyle("-fx-alignment: center; -fx-background-color: #b59f3a;");
+                } else {
+                    letter.setStyle("-fx-alignment: center; -fx-background-color: #3a3b3c;");
+                }
+            }
+            check3 = false;
+            check4 = true;
         } else if(check4) {
+            for (int i = 0; i < guess.length() - 1; i++) {
+                Label letter = getLabelByIndex(i+15);
 
+                if(guess.charAt(i) == chosenWord.charAt(i)){
+                    letter.setStyle("-fx-alignment: center; -fx-background-color: #528d4e;");
+                } else if(chosenWord.indexOf(guess.substring(i, i+1)) != -1){
+                    letter.setStyle("-fx-alignment: center; -fx-background-color: #b59f3a;");
+                } else {
+                    letter.setStyle("-fx-alignment: center; -fx-background-color: #3a3b3c;");
+                }
+            }
+            check4 = false;
+            check5 = true;
         } else if(check5) {
+            for (int i = 0; i < guess.length() - 1; i++) {
+                Label letter = getLabelByIndex(i+20);
 
+                if(guess.charAt(i) == chosenWord.charAt(i)){
+                    letter.setStyle("-fx-alignment: center; -fx-background-color: #528d4e;");
+                } else if(chosenWord.indexOf(guess.substring(i, i+1)) != -1){
+                    letter.setStyle("-fx-alignment: center; -fx-background-color: #b59f3a;");
+                } else {
+                    letter.setStyle("-fx-alignment: center; -fx-background-color: #3a3b3c;");
+                }
+            }
+            check5 = false;
+            check6 = true;
         } else if (check6) {
+            for (int i = 0; i < guess.length() - 1; i++) {
+                Label letter = getLabelByIndex(i+25);
+
+                if(guess.charAt(i) == chosenWord.charAt(i)){
+                    letter.setStyle("-fx-alignment: center; -fx-background-color: #528d4e;");
+                } else if(chosenWord.indexOf(guess.substring(i, i+1)) != -1){
+                    letter.setStyle("-fx-alignment: center; -fx-background-color: #b59f3a;");
+                } else {
+                    letter.setStyle("-fx-alignment: center; -fx-background-color: #3a3b3c;");
+                }
+            }
+            check6 = false;
+        } else {
 
         }
     }
 
-    public void setAllVisible() {
+    private void setAllVisible() {
         gridPane.setOpacity(1);
         btnGuess.setOpacity(1);
         txtGuess.setOpacity(1);
         label.setOpacity(1);
+    }
+
+    private Label getLabelByIndex(int i){
+        switch (i) {
+            case 0:
+                return letter1;
+            case 1:
+                return letter2;
+            case 2:
+                return letter3;
+            case 3:
+                return letter4;
+            case 4:
+                return letter5;
+            case 5:
+                return letter6;
+            case 6:
+                return letter7;
+            case 7:
+                return letter8;
+            case 8:
+                return letter9;
+            case 9:
+                return letter10;
+            case 10:
+                return letter11;
+            case 11:
+                return letter12;
+            case 12:
+                return letter13;
+            case 13:
+                return letter14;
+            case 14:
+                return letter15;
+            case 15:
+                return letter16;
+            case 16:
+                return letter17;
+            case 17:
+                return letter18;
+            case 18:
+                return letter19;
+            case 19:
+                return letter20;
+            case 20:
+                return letter21;
+            case 21:
+                return letter22;
+            case 22:
+                return letter23;
+            case 23:
+                return letter24;
+            case 24:
+                return letter25;
+            case 25:
+                return letter26;
+            case 26:
+                return letter27;
+            case 27:
+                return letter28;
+            case 28:
+                return letter29;
+            case 29:
+                return letter30;
+            default:
+                return null;
+        }
     }
 }
--- app/src/main/java/finalproj/wordleTxtController.java
+++ app/src/main/java/finalproj/wordleTxtController.java
@@ -14,6 +14,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonBar;
 import javafx.scene.control.ButtonType;
+import javafx.scene.control.Dialog;
 
 public class wordleTxtController {
     @FXML
